ecc2f892f8b9a10292e770f542d78eb4a1c89285
Markus Stracke <mstracke@c4c3c1.42berlin.de>
2025-03-04 12:29:49 +0100

    style[error_handling.c]: improve readability and func length

--- srcs/error_handling.c
+++ srcs/error_handling.c
@@ -6,19 +6,13 @@
 /*   By: mstracke <mstracke@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:42:23 by mstracke          #+#    #+#             */
-/*   Updated: 2025/02/26 16:06:39 by mstracke         ###   ########.fr       */
+/*   Updated: 2025/03/04 12:28:12 by mstracke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/**
- * @brief function to organise error handling
- * print error messages and free allocated memory
- * 
- * @param err own error-code
- */
-void	ft_error_handling(int err, char *addinfo, t_gamedata *config)
+void	ft_error_parsing_1(int err, char *addinfo)
 {
 	if (err == 0)
 	{
@@ -44,9 +38,11 @@ void	ft_error_handling(int err, char *addinfo, t_gamedata *config)
 		ft_dprintf(2, "Map input is wrong. Wrong character(s).\n");
 	else if (err == 8)
 		ft_dprintf(2, "There are more than one idenitifer for %s.\n", addinfo);
-	else if (err == 9)
-		ft_dprintf(2, "Malloc problems.\n");
-	else if (err == 10)
+}
+
+void	ft_error_parsing_2(int err, char *addinfo)
+{
+	if (err == 10)
 		ft_dprintf(2, "The map is not surrounded by walls in %s.\n", addinfo);
 	else if (err == 11)
 		ft_dprintf(2, "How will you have fun without "
@@ -54,6 +50,22 @@ void	ft_error_handling(int err, char *addinfo, t_gamedata *config)
 	else if (err == 12)
 		ft_dprintf(2, "How will you play with several players "
 			"at the same time, idiot? Check your map, it is invalid!\n");
+}
+
+/**
+ * @brief function to organise error handling
+ * print error messages and free allocated memory
+ * 
+ * @param err own error-code
+ */
+void	ft_error_handling(int err, char *addinfo, t_gamedata *config)
+{
+	if (err < 0)
+		ft_error_parsing_1(err, addinfo);
+	else if (err == 9)
+		ft_dprintf(2, "Malloc problems.\n");
+	else if (err > 9 && err < 20)
+		ft_error_parsing_2(err, addinfo);
 	if (config)
 		ft_free_config(config);
 	if (addinfo)
